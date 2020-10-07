dd197825f0ced7243d9e5d4e3da95380f5dc5467
Dmitry Ledentsov <dmnled@gmail.com>
2020-10-07 13:43:03 +0200

    cleanup

--- cmd/awslambda.go
+++ cmd/awslambda.go
@@ -23,16 +23,9 @@ func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.API
 	return ginLambda.ProxyWithContext(ctx, req)
 }
 
-// awsLambdaCmd represents the awsLambda command
 var awsLambdaCmd = &cobra.Command{
 	Use:   "awslambda",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "start the service as an AWS Lambda",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("awsLambda called")
 		fetchConfig()
@@ -51,14 +44,4 @@ to quickly create a Cobra application.`,
 
 func init() {
 	serveCmd.AddCommand(awsLambdaCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// awsLambdaCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// awsLambdaCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
